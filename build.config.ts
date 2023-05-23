735219dad05ddbbb76775397779cc7deb70ff714
antrioe <antrioe@gmali.com>
2023-05-23 11:57:28 +0800

    fix

--- build.config.ts
+++ build.config.ts
@@ -1,12 +1,8 @@
-import { resolve } from 'node:path'
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   declaration: true,
   entries: ['src/index'],
-  alias: {
-    '~': resolve(__dirname, './src'),
-  },
   rollup: {
     emitCJS: true,
     inlineDependencies: true,
