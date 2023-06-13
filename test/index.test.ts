c8eadcf768c5acdbee87863e6f310f98122a4fe5
antrioe <antrioe@gmali.com>
2023-06-13 14:19:31 +0800

    ci(deps): 依赖更新

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,9 @@
 import { describe, expect, it } from 'vitest'
+import { one, two } from '../src/index'
 
 describe('should', () => {
   it('exported', () => {
-    expect(1).toEqual(1)
+    expect(one).toEqual(1)
+    expect(two).toEqual(2)
   })
 })
