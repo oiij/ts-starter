02b08916ac4561b30b2d4783bd07574777bfcdd6
antrioe <antrioe@gmali.com>
2025-04-22 14:58:39 +0800

    ci(deps): ci

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['./src/index.ts'],
-  clean: true,
   format: ['cjs', 'esm'],
-  external: ['vue'],
+  clean: true,
   dts: true,
-  minify: false,
 })
